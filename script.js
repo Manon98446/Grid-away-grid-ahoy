d6ee6bb78ac5dc9bed97082e7a26eeba3efb8752
Manon <manon.vdm@hotmail.com>
2023-05-26 12:24:55 +0200

    starting sidebar

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function sidebar_open() {
+    document.getElementById("my_sidebar").style.display = "block";
+    document.getElementById("my_overlay").style.display = "block";
+  }
+  
+  function sidebar_close() {
+    document.getElementById("my_sidebar").style.display = "none";
+    document.getElementById("my_overlay").style.display = "none";
+  }
